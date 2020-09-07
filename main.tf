a4eb55ba3d135a4c9cceb723e5e0fc3af13a5116
Jan Jungnickel <jan@jungnickel.com>
2020-09-07 10:37:37 +0200

    Terraform 0.13

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "admin_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.12.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.admin_name
@@ -9,7 +9,7 @@ module "admin_label" {
 }
 
 module "readonly_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.12.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.readonly_name
